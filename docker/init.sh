e1922d92fb203407ef7b847e9fed9d2e7b84e6f7
Quin-Darcy <pohmsuindraguli@gmail.com>
2025-05-04 16:25:55 -0500

    Update docker scripts

--- docker/init.sh
+++ docker/init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build --build-arg USER_UID=$(id -u) --build-arg USER_GID=$(id -g) -t kernel-dev .
+export UID=$(id -u)
+export GID=$(id -g)
+
+docker compose build
+docker compose run --rm kernel-build
